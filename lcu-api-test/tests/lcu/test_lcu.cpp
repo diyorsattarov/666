349cea4971182c8ecfa8942e3cc45d399669dfe4
diyooor <diyorsattarov7@gmail.com>
2023-11-22 12:01:11 -0600

    Tightened test up and passes successfully with expected results.

--- lcu-api-test/tests/lcu/test_lcu.cpp
+++ lcu-api-test/tests/lcu/test_lcu.cpp
@@ -10,25 +10,34 @@ protected:
     void TearDown() override {
     }
     LCU lcu;
-    std::string endpoint = "";
+    std::string endpoint;
+    std::string jsonPayload;
+
     cpr::Header headers{
         {"accept", "application/json"},
         {"Authorization", "Basic cmlvdDpyNEZ3TjJoVHJKUllPTTZDaGxBczRB"},
         {"Content-Type", "application/json"}
     };
-    std::string jsonPayload = R"(
+};
+
+TEST_F(LCUTest, PutRequestTest) {
+    jsonPayload = R"(
         {
             "inventoryToken": "string",
             "profileIconId": 29
         }
     )";
-};
-
-TEST_F(LCUTest, PutRequestTest) {
     endpoint = "https://127.0.0.1:59284/lol-summoner/v1/current-summoner/icon";
     std::string response = lcu.Request("PUT", endpoint, headers, jsonPayload);
+    Json::CharReaderBuilder reader;
+    Json::Value root;
+    std::istringstream response_stream(response);
+    Json::parseFromStream(reader, response_stream, &root, nullptr);
 
-    std::cout << response << std::endl;
-
-    ASSERT_TRUE(response.find("expected_response_string") != std::string::npos);
+    if (root.isObject() && root.isMember("profileIconId")) {
+        ASSERT_EQ(root["profileIconId"], 29);
+    }
+    else {
+        std::cerr << "Invalid JSON response." << std::endl;
+    }
 }
